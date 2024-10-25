3c783b8bb6c2fcccc25ae813372c87e6891bebb2
root <root@UDAY>
2024-10-25 15:30:50 +0530

    added Favicon and experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,33 +9,13 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "jan 2024 - Present",
+    role: "Webdevelopment Trainee",
+    company: "100X Devs.",
+    description: `I am a trainee at 100x Devs, where I am gaining hands-on experience with modern web development tools and technologies. My learning focuses on the MRN stack (MongoDB, React, Node.js) alongside TypeScript, PostgreSQL, Prisma, and Docker. Additionally, I have skills in Golang, which I apply to develop efficient backend solutions. I also enhance frontend designs using Tailwind CSS and explore Next.js for server-side rendering and advanced React.js patterns to build robust, scalable applications.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB","Tailwind Css","Golang","ExpressJs","Docker"],
   },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+  
 ];
 
 export const PROJECTS = [
